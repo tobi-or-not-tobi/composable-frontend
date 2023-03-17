0f5c24380cc04a203abead8545cd00d5f665374c
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2023-03-17 18:30:30 +0100

    custom colors

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,11 @@ export const app = appBuilder()
   .withEnvironment(import.meta.env)
   .withFeature(b2cFeatures)
   .withTheme(storefrontTheme)
+
+  // some custom theme colors
+  .withTheme({
+    name: 'demo',
+    designTokens: [{ color: { primary: { 300: 'hotpink' } } }],
+  })
+
   .create();
